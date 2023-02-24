01b5021289875770dfeb64636e109a499236879e
minju412 <ln8847@ajou.ac.kr>
2023-02-24 13:31:43 +0900

    - 파일 하나가 아닌 여러개를 업로드하기 위해 List<MultipartFile> 사용

--- src/main/java/com/dasibom/practice/domain/DiaryImage.java
+++ src/main/java/com/dasibom/practice/domain/DiaryImage.java
@@ -8,8 +8,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import lombok.Setter;
 
 @Entity
+@Setter
 public class DiaryImage {
 
     @Id
