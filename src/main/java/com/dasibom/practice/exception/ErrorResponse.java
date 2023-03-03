8a9f7541b38f3809822b1a0913d6955d47685625
minju412 <ln8847@ajou.ac.kr>
2023-03-03 16:06:55 +0900

    #13 feat: error response에 status code 추가

--- src/main/java/com/dasibom/practice/exception/ErrorResponse.java
+++ src/main/java/com/dasibom/practice/exception/ErrorResponse.java
@@ -3,29 +3,34 @@ package com.dasibom.practice.exception;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.http.ResponseEntity;
+
 @Getter
 @Builder
 public class ErrorResponse {
-    private final String error;
-    private final String message;
+
+    private int statusCode; // 404
+    private final String error; // NOT_FOUND
+    private final String message; // 예외 메시지
 
     public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode) {
         return ResponseEntity
-            .status(errorCode.getHttpStatus())
-            .body(ErrorResponse.builder()
-                .error(errorCode.getHttpStatus().name())
-                .message(errorCode.getMessage())
-                .build()
-            );
+                .status(errorCode.getHttpStatus())
+                .body(ErrorResponse.builder()
+                        .statusCode(errorCode.getHttpStatus().value())
+                        .error(errorCode.getHttpStatus().name())
+                        .message(errorCode.getMessage())
+                        .build()
+                );
     }
 
     public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode, String defaultMessage) {
         return ResponseEntity
-            .status(errorCode.getHttpStatus())
-            .body(ErrorResponse.builder()
-                .error(errorCode.getHttpStatus().name())
-                .message(defaultMessage)
-                .build()
-            );
+                .status(errorCode.getHttpStatus())
+                .body(ErrorResponse.builder()
+                        .statusCode(errorCode.getHttpStatus().value())
+                        .error(errorCode.getHttpStatus().name())
+                        .message(defaultMessage)
+                        .build()
+                );
     }
 }
