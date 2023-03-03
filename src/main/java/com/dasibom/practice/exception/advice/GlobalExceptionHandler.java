998395d47ae840de027a7ec251bf30986cf57c21
minju412 <ln8847@ajou.ac.kr>
2023-03-03 16:27:42 +0900

    #13 style: 불필요한 코드 제거

--- src/main/java/com/dasibom/practice/exception/advice/GlobalExceptionHandler.java
+++ src/main/java/com/dasibom/practice/exception/advice/GlobalExceptionHandler.java
@@ -7,7 +7,6 @@ import static com.dasibom.practice.exception.ErrorCode.METHOD_ARG_NOT_VALID;
 import com.dasibom.practice.exception.CustomException;
 import com.dasibom.practice.exception.ErrorResponse;
 import java.util.Objects;
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -20,22 +19,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    // @Valid 검증 실패 시 Catch
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected Object handleMethodArgumentNotValidException(MethodArgumentNotValidException ex,
-            HttpServletRequest request) {
+    protected Object handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         String defaultMessage = Objects.requireNonNull(ex.getBindingResult().getFieldError()).getDefaultMessage();
         return ErrorResponse.toResponseEntity(METHOD_ARG_NOT_VALID, defaultMessage);
     }
 
+    // 제약 조건 위배 시 Catch
     @ExceptionHandler(value = {ConstraintViolationException.class, DataIntegrityViolationException.class})
     protected ResponseEntity<ErrorResponse> handleDataException() {
-        log.error("handleDataException throw Exception : {}", CONSTRAINT_VIOLATION);
         return ErrorResponse.toResponseEntity(CONSTRAINT_VIOLATION);
     }
 
+    // CustomException 을 상속받은 클래스가 예외를 발생 시킬 시, Catch 하여 ErrorResponse 를 반환한다.
     @ExceptionHandler(CustomException.class)
     protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
-        log.error("handleCustomException throw CustomException : {}", e.getErrorCode());
         return ErrorResponse.toResponseEntity(e.getErrorCode());
     }
 }
