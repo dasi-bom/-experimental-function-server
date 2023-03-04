36cd5ee0eb35c3857e0fef9b2471b77f161f71cd
minju412 <ln8847@ajou.ac.kr>
2023-03-05 00:01:33 +0900

    #17 fix: cors 설정 변경

---
+++ src/main/java/com/dasibom/practice/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.dasibom.practice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://localhost:3000",
+                        "http://localhost:8080",
+                        "https://localhost:8080"
+                )
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+
+}
