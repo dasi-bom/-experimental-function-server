7bfa82bfe65cf241f9ccc5be4169cfd0db2f5051
minju412 <ln8847@ajou.ac.kr>
2023-03-04 11:33:40 +0900

    - cursor 기반 무한 스크롤 적용

--- src/main/java/com/dasibom/practice/repository/DiaryRepository.java
+++ src/main/java/com/dasibom/practice/repository/DiaryRepository.java
@@ -1,8 +1,9 @@
 package com.dasibom.practice.repository;
 
 import com.dasibom.practice.domain.Diary;
+import com.dasibom.practice.repository.custom.CustomDiaryRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DiaryRepository extends JpaRepository<Diary, Long> {
+public interface DiaryRepository extends JpaRepository<Diary, Long>, CustomDiaryRepository {
 
 }
--- src/main/java/com/dasibom/practice/service/DiaryService.java
+++ src/main/java/com/dasibom/practice/service/DiaryService.java
@@ -1,13 +1,19 @@
 package com.dasibom.practice.service;
 
+import com.dasibom.practice.condition.DiaryReadCondition;
 import com.dasibom.practice.domain.Diary;
+import com.dasibom.practice.dto.DiaryBriefInfoDto;
 import com.dasibom.practice.dto.DiaryDetailResDto;
 import com.dasibom.practice.dto.DiarySaveReqDto;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 public interface DiaryService {
 
     Diary save(DiarySaveReqDto requestDto);
 
     DiaryDetailResDto getDetailedDiary(Long diaryId);
+
+    Slice<DiaryBriefInfoDto> getDiaryList(Long cursor, DiaryReadCondition condition, Pageable pageRequest);
 
 }
