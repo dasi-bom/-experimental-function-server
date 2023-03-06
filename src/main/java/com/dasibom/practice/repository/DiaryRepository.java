7b3ed77c1e24dad975d944fc66909d960ab9aecf
minju412 <ln8847@ajou.ac.kr>
2023-03-06 23:54:04 +0900

    #24 comment: 주석 추가

--- src/main/java/com/dasibom/practice/repository/DiaryRepository.java
+++ src/main/java/com/dasibom/practice/repository/DiaryRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long>, CustomDiaryRepository {
 
+    // Entity 테이블을 id 역순으로 정렬한 뒤 첫 로우 리턴
     Diary findFirstByOrderByIdDesc();
 }
