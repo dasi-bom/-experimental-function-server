0ae3fc6fdba7973fdc518d765fc45a0a8991b640
minju412 <ln8847@ajou.ac.kr>
2023-03-06 15:38:59 +0900

    #20 feat: 일기 등록과 파일 업로드 api 분리

--- src/main/java/com/dasibom/practice/exception/ErrorCode.java
+++ src/main/java/com/dasibom/practice/exception/ErrorCode.java
@@ -13,6 +13,7 @@ public enum ErrorCode {
     METHOD_ARG_NOT_VALID(HttpStatus.BAD_REQUEST, "제약 조건을 위배한 요청입니다 (method argument not valid)"),
     STAMP_LIST_SIZE_ERROR(HttpStatus.BAD_REQUEST, "최대 3개의 스탬프를 선택할 수 있습니다"),
     INVALID_FILE_ERROR(HttpStatus.BAD_REQUEST, "확장자가 jpg, jpeg, png 인 파일만 업로드 가능합니다."),
+    FILE_NOT_EXIST_ERROR(HttpStatus.BAD_REQUEST, "파일이 존재하지 않습니다."),
 
     /* 404 NOT_FOUND : Resource 를 찾을 수 없음 */
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다"),
